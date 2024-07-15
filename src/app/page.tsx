8d0ada46202a522344eb3706b283324748eba664
somebodyda <kiyalege2@gmail.com>
2024-07-15 13:33:25 +0300

    using useCalleback for coffee cup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 
 function CoffeeClock() {
   type ClockDegrees = {
@@ -8,45 +8,47 @@ function CoffeeClock() {
     hoursDegree: number;
   };
 
-  useEffect(() => {
-    const updateClock = () => {
-      // "test"
-      const coffeeClockElement = document.querySelector(
-        ".coffee-clock"
-      ) as HTMLElement;
-      console.clear();
-      const date = new Date();
-      const hours = date.getHours();
-      const minutes = date.getMinutes();
-      const seconds = date.getSeconds();
+  const updateClock = useCallback(() => {
+    // "test"
+    const coffeeClockElement = document.querySelector(
+      ".coffee-clock"
+    ) as HTMLElement;
+    console.clear();
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 
-      const degrees: ClockDegrees = {
-        secondsDegree: (360 / 60) * seconds,
-        minutesDegree: (360 / 60) * minutes + ((360 / 60) * seconds) / 60,
-        hoursDegree:
-          (360 / 12) * (hours % 12 || 12) + ((360 / 60) * minutes) / 60,
-      };
-
-      console.log(degrees);
-
-      coffeeClockElement.style.setProperty(
-        "--angle-hour",
-        `${degrees.hoursDegree}deg`
-      );
-      coffeeClockElement.style.setProperty(
-        "--angle-minute",
-        `${degrees.minutesDegree}deg`
-      );
-      coffeeClockElement.style.setProperty(
-        "--angle-second",
-        `${degrees.secondsDegree}deg`
-      );
+    const degrees: ClockDegrees = {
+      secondsDegree: (360 / 60) * seconds,
+      minutesDegree: (360 / 60) * minutes + ((360 / 60) * seconds) / 60,
+      hoursDegree:
+        (360 / 12) * (hours % 12 || 12) + ((360 / 60) * minutes) / 60,
     };
+
+    console.log(degrees);
+
+    coffeeClockElement.style.setProperty(
+      "--angle-hour",
+      `${degrees.hoursDegree}deg`
+    );
+    coffeeClockElement.style.setProperty(
+      "--angle-minute",
+      `${degrees.minutesDegree}deg`
+    );
+    coffeeClockElement.style.setProperty(
+      "--angle-second",
+      `${degrees.secondsDegree}deg`
+    );
+  }, []);
+
+  useEffect(() => {
+    updateClock();
 
     const intervalId = setInterval(updateClock, 60000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [updateClock]);
   return (
     <div className="coffee-clock">
       <div className="plate"></div>
