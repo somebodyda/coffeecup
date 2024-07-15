e489b2dc2fd88c1de6704a2a3ab8684d8238f8ff
somebodyda <kiyalege2@gmail.com>
2024-07-15 13:23:49 +0300

    fixed error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,52 @@
 "use client";
 import React, { useEffect } from "react";
 
-function coffeeClock() {
+function CoffeeClock() {
   type ClockDegrees = {
     secondsDegree: number;
     minutesDegree: number;
     hoursDegree: number;
   };
 
-  const updateClock = () => {
-    // "test"
-    const coffeeClockElement = document.querySelector(".coffee-clock");
-    console.clear();
-    const date = new Date();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-
-    const degrees: ClockDegrees = {
-      secondsDegree: (360 / 60) * seconds,
-      minutesDegree: (360 / 60) * minutes + ((360 / 60) * seconds) / 60,
-      hoursDegree:
-        (360 / 12) * (hours % 12 || 12) + ((360 / 60) * minutes) / 60,
-    };
-
-    console.log(degrees);
-
-    coffeeClockElement.style.setProperty(
-      "--angle-hour",
-      `${degrees.hoursDegree}deg`
-    );
-    coffeeClockElement.style.setProperty(
-      "--angle-minute",
-      `${degrees.minutesDegree}deg`
-    );
-    coffeeClockElement.style.setProperty(
-      "--angle-second",
-      `${degrees.secondsDegree}deg`
-    );
-  };
-
   useEffect(() => {
-    updateClock();
+    const updateClock = () => {
+      // "test"
+      const coffeeClockElement = document.querySelector(
+        ".coffee-clock"
+      ) as HTMLElement;
+      console.clear();
+      const date = new Date();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      const seconds = date.getSeconds();
+
+      const degrees: ClockDegrees = {
+        secondsDegree: (360 / 60) * seconds,
+        minutesDegree: (360 / 60) * minutes + ((360 / 60) * seconds) / 60,
+        hoursDegree:
+          (360 / 12) * (hours % 12 || 12) + ((360 / 60) * minutes) / 60,
+      };
+
+      console.log(degrees);
+
+      coffeeClockElement.style.setProperty(
+        "--angle-hour",
+        `${degrees.hoursDegree}deg`
+      );
+      coffeeClockElement.style.setProperty(
+        "--angle-minute",
+        `${degrees.minutesDegree}deg`
+      );
+      coffeeClockElement.style.setProperty(
+        "--angle-second",
+        `${degrees.secondsDegree}deg`
+      );
+    };
 
     const intervalId = setInterval(updateClock, 60000);
 
     return () => clearInterval(intervalId);
-  }, [updateClock]);
+  }, []);
   return (
     <div className="coffee-clock">
       <div className="plate"></div>
@@ -62,4 +62,4 @@ function coffeeClock() {
   );
 }
 
-export default coffeeClock;
+export default CoffeeClock;
